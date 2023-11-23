7df1ac9b626d9d239b5c147e2b9bb4c2805c16a1
Your Name <you@example.com>
2023-11-23 12:53:00 +0700

    removed node_modules folder

---
+++ index.js
@@ -0,0 +1,37 @@
+const PublicGoogleSheetsParser = require('public-google-sheets-parser')
+const express = require('express')
+const app = express()
+const port = 3000
+
+// const spreadsheetId = '14GWuOexHCX9-t9Q6VWkIspWQIbCO96xW5JG59cGWVb8'
+const parser = new PublicGoogleSheetsParser()
+
+app.get('/getvideos/:id', (req, res) => {
+    parser.parse(req.params.id).then((items) => {
+        var videos = items.map(item => item.videos)
+        var myjson ={
+            videos: videos
+        }
+        console.log(myjson)
+        res.send(myjson)
+      })
+    
+
+})
+
+
+app.get('/getjson/:id', (req, res) => {
+    parser.parse(req.params.id).then((items) => {       
+        res.send(items[0])
+      })
+    
+
+})
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
